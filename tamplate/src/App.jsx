8f6201e8fb2fe4619aff6c13d06ce6e570ec08f9
Theng Rachana <vaingglorywar@gmail.com>
2023-12-17 22:49:41 +0700

    complete api with update ui

--- tamplate/src/App.jsx
+++ tamplate/src/App.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-function App() {
+import {v4 as uuidv4} from 'uuid';
+const App = () => {
   const [statusList, setStatusList] = useState([]);
+  const [newStatus, setNewStatus] = useState('');
 
   useEffect(() => {
-    // Fetch data from the Node.js API when the component mounts
     fetchData();
   }, []);
 
   const fetchData = async () => {
     try {
-      // Replace 'http://localhost:5000' with the actual URL of your Node.js server
       const response = await axios.get('http://localhost:5000/status');
       setStatusList(response.data);
     } catch (error) {
@@ -19,18 +18,60 @@ function App() {
     }
   };
 
+  const handleAddStatus = async () => {
+    try {
+      const statusWithoutId = statusList.map(({ id, ...rest }) => rest);
+
+      const newId = uuidv4();
+
+      const newStatusItem = {
+        id: newId,
+        name: newStatus,
+      };
+
+      await axios.post('http://localhost:5000/status', newStatusItem);
+
+      setStatusList([...statusWithoutId, newStatusItem]);
+
+      setNewStatus('');
+    } catch (error) {
+      console.error('Error adding new status:', error);
+    }
+  };
+
+  const handleDeleteStatus = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/status/${id}`);
+
+      setStatusList(statusList.filter((status) => status.id !== id));
+    } catch (error) {
+      console.error('Error deleting status:', error);
+    }
+  };
+
   return (
-    <div className="App">
+    <div>
       <h1>Status List</h1>
       <ul>
         {statusList.map((status) => (
           <li key={status.id}>
-            <strong>ID:</strong> {status.id}, <strong>Name:</strong> {status.name}
+            {status.name}
+            <button onClick={() => handleDeleteStatus(status.id)}>Delete</button>
           </li>
         ))}
       </ul>
+      <div>
+        <h2>Add New Status</h2>
+        <input
+          type="text"
+          placeholder="Enter status name"
+          value={newStatus}
+          onChange={(e) => setNewStatus(e.target.value)}
+        />
+        <button onClick={handleAddStatus}>Add Status</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
